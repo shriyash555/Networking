47eed739e959cb4f560a9683e0e8891a2e2b0344
Raunak Singwi <raunaksingwi7@gmail.com>
2017-07-30 16:19:15 +0530

    I hope it gets restored

--- chat_client.py
+++ chat_client.py
@@ -1,5 +1,22 @@
 import socket
-import sys #to quit the program
+import thread #for multithreading
+
+def send_message():
+  while(True):
+    message = raw_input("Me : ")
+    message = "Client : " + message
+    chat_client.sendall(message)
+    
+    if "bye" in responce.lower():
+      exit()
+
+def receive_message():
+  while(True):
+    responce = chat_client.recv(4096)
+    print responce
+
+    if "bye" in responce.lower():
+      exit()
 
 chat_client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host = "localhost"
@@ -7,6 +24,14 @@ host_ip = socket.gethostbyname(host)
 port = 1234
 chat_client.connect((host_ip,port))
 
+try:
+  thread.start_new_thread(send_message, ())
+  thread.start_new_thread(receive_message, ())
+except Exception,msg:
+  print "something bad happen to client that says"
+  print msg
+
+"""
 while(True):
   #TODO client thread no. 1 will handle sending of message from client to server
   message = raw_input("Me : ")
@@ -23,23 +48,5 @@ while(True):
   if "bye" in responce.lower():
     break
 
-chat_client.close()
+chat_client.close()"""
 
-"""
-def send_message():
-  while(True):
-    message = raw_input("Me : ")
-    message = "Client : " + message
-    chat_client.sendall(message)
-    
-    if "bye" in responce.lower():
-      sys.exit(0)
-
-def receive_message():
-  while(True):
-    responce = chat_client.recv(4096)
-    print responce
-
-    if "bye" in responce.lower():
-      sys.exit(0)
-"""
--- chat_server.py
+++ chat_server.py
@@ -1,4 +1,21 @@
 import socket
+import thread
+
+def receive_message():
+  while(True):
+    message = conn.recv(4096)
+    print message
+
+    if "bye" in message.lower():
+      exit()
+
+def send_message():
+  responce = raw_input("Me : ")
+  responce = "Server : " + responce
+  conn.sendall(responce)
+
+  if "bye" in responce.lower():
+    exit()
 
 chat_server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host = "localhost"
@@ -8,6 +25,14 @@ chat_server.bind((host_ip,port))
 chat_server.listen(1)
 conn , addr = chat_server.accept()
 
+try:
+  thread.start_new_thread(receive_message, ())
+  thread.start_new_thread(send_message, ())
+except Exception,msg:
+  print "something bad happen to server that says"
+  print mgs
+
+"""
 while(True):
 
   #TODO server thread no. 1 will handle receiving message from client.
@@ -25,22 +50,5 @@ while(True):
   if "bye" in responce.lower():
     break
 
-chat_server.close()
+chat_server.close()"""
 
-"""
-def receive_message():
-  while(True):
-    message = conn.recv(4096)
-    print message
-
-    if "bye" in message.lower():
-      sys.exit(0)
-
-def send_message():
-  responce = raw_input("Me : ")
-  responce = "Server : " + responce
-  conn.sendall(responce)
-
-  if "bye" in responce.lower():
-    sys.exit(0)
-"""
